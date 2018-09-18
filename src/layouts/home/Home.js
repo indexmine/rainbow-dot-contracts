deb2f9e80611cd7a17bdb1920d04dddae8be7a14
James Lim <email@wanseob.com>
2018-09-18 19:08:55 +0900

    Restructure with drizzle box

---
+++ src/layouts/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { AccountData, ContractData, ContractForm } from 'drizzle-react-components'
+import logo from '../../logo.png'
+
+class Home extends Component {
+  render() {
+    return (
+      <main className="container">
+      </main>
+    )
+  }
+}
+
+export default Home
